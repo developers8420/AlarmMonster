9e510935f0fe93360ee10b5c63d78772af9a3bab
植原　駿 <ueponmasuta@gmail.com>
2015-10-24 14:22:44 +0900

    リポジトリ作成 AlarmMonster

---
+++ AlarmMonster/AlarmMonster/AlarmMonster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlarmMonster
+//
+//  Created by 植原　駿 on 2015/10/24.
+//  Copyright (c) 2015年 Shun Uehara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
